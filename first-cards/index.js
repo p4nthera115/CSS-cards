3f430cf4958239f4a3ff9a899702db97eda7dcc7
p4nthera115 <asdshuja@hotmail.co.uk>
2023-05-02 16:01:28 +0100

    open and close

--- first-cards/index.js
+++ first-cards/index.js
@@ -1,18 +1,28 @@
+const cards = document.getElementsByClassName("card-group");
+
 const first = document.querySelector(".card-group :nth-child(1)");
 const second = document.querySelector(".card-group :nth-child(2)");
 const third = document.querySelector(".card-group :nth-child(3)");
 const fourth = document.querySelector(".card-group :nth-child(4)");
 
-const clickCards = () => {
-  first.style.transform = "translate(-300%, 10%)";
-  second.style.transform = "translate(-133%, 10%)";
-  third.style.transform = "translate(33%, 10%)";
-  fourth.style.transform = "translate(200%, 10%)";
-};
+console.log(cards[0].attributes[2].value);
 
-const closeCards = () => {
-  first.style.transform = "";
-  second.style.transform = "";
-  third.style.transform = "";
-  fourth.style.transform = "";
+const clickCards = () => {
+  if (cards[0].attributes[2].value === "inactive") {
+    cards[0].attributes[2].value = "active";
+
+    first.style.transform = "translate(-300%, 10%)";
+    second.style.transform = "translate(-133%, 10%)";
+    third.style.transform = "translate(33%, 10%)";
+    fourth.style.transform = "translate(200%, 10%)";
+  } else if (cards[0].attributes[2].value == "active") {
+    cards[0].attributes[2].value = "inactive";
+
+    first.style.transform = "";
+    second.style.transform = "";
+    third.style.transform = "";
+    fourth.style.transform = "";
+  }
+
+  console.log(cards[0].attributes[2].value);
 };
