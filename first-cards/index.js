2ad025db5c4a8c8d3356e3de3e8f6fcf55ba7162
p4nthera115 <asdshuja@hotmail.co.uk>
2023-05-02 18:06:02 +0100

    need to add on hover playGif

--- first-cards/index.js
+++ first-cards/index.js
@@ -1,28 +1,46 @@
-const cards = document.getElementsByClassName("card-group");
+const cardsGroup = document.getElementsByClassName("card-group");
 
 const first = document.querySelector(".card-group :nth-child(1)");
 const second = document.querySelector(".card-group :nth-child(2)");
 const third = document.querySelector(".card-group :nth-child(3)");
 const fourth = document.querySelector(".card-group :nth-child(4)");
 
-console.log(cards[0].attributes[2].value);
+console.log(cardsGroup[0].attributes[2].value);
 
 const clickCards = () => {
-  if (cards[0].attributes[2].value === "inactive") {
-    cards[0].attributes[2].value = "active";
+  if (cardsGroup[0].attributes[2].value === "inactive") {
+    cardsGroup[0].attributes[2].value = "active";
 
-    first.style.transform = "translate(-300%, 10%)";
-    second.style.transform = "translate(-133%, 10%)";
-    third.style.transform = "translate(33%, 10%)";
-    fourth.style.transform = "translate(200%, 10%)";
-  } else if (cards[0].attributes[2].value == "active") {
-    cards[0].attributes[2].value = "inactive";
+    first.style = "transform: translate(-300%, 10%)";
+    first.children[0].style =
+      "opacity: 1; transition: opacity 0.3s ease-in-out;";
 
-    first.style.transform = "";
-    second.style.transform = "";
-    third.style.transform = "";
-    fourth.style.transform = "";
+    second.style = "transform: translate(-133%, 10%)";
+    second.children[0].style =
+      "opacity: 1; transition: opacity 0.3s ease-in-out;";
+
+    third.style = "transform: translate(33%, 10%)";
+    third.children[0].style =
+      "opacity: 1; transition: opacity 0.3s ease-in-out;";
+
+    fourth.style = "transform: translate(200%, 10%)";
+    fourth.children[0].style =
+      "opacity: 1; transition: opacity 0.3s ease-in-out;";
+  } else if (cardsGroup[0].attributes[2].value === "active") {
+    cardsGroup[0].attributes[2].value = "inactive";
+
+    first.style = "";
+    first.children[0].style = "opacity: 0;";
+
+    second.style = "";
+    second.children[0].style = "opacity: 0;";
+
+    third.style = "";
+    third.children[0].style = "opacity: 0;";
+
+    fourth.style = "";
+    fourth.children[0].style = "opacity: 0;";
   }
 
-  console.log(cards[0].attributes[2].value);
+  console.log(cardsGroup[0].attributes[2].value);
 };
